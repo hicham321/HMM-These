64353d54c7c06f2fff507162455081eb7dafb369
hicham321 <gherbixhicham@gmail.com>
2015-05-11 17:43:01 +0200

    this is tf idf skeleton

---
+++ src/tf_idf.java
@@ -0,0 +1,7 @@
+// this is a class to implement an if idf on the documents 
+//the result is a vector of the tf idf values for each word
+//from this we can then extract the keywords and related keywords in the texts 
+
+public class tf_idf {
+
+}
