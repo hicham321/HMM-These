e47fce20a26413998860de9eb37ae1bdcf209316
hicham321 <gherbixhicham@gmail.com>
2015-05-11 17:33:23 +0200

    file iteration

--- src/FileUriAndUrl.java
+++ src/FileUriAndUrl.java
@@ -5,6 +5,22 @@ import java.net.URI;
 import java.net.URL;
 
 public class FileUriAndUrl {
+	
+	public void iterateFile(){
+		File dir = new File("myDirectoryPath");
+		  File[] directoryListing = dir.listFiles();
+		  if (directoryListing != null) {
+		    for (File child : directoryListing) {
+		      // Do something with child
+		    }
+		  } else {
+		    // Handle the case where dir is not really a directory.
+		    // Checking dir.isDirectory() above would not be sufficient
+		    // to avoid race conditions with another process that deletes
+		    // directories.
+		  }
+		
+	}
 
 	public static void main(String[] args) throws IOException {
 
