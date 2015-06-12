b91f448e8b6c5d71cdcdbea9cc4ad5df7046c1b1
Gherbi Hicham <gherbixhicham@gmail.com>
2015-06-12 23:17:36 +0100

    classification class

--- src/Classification.java
+++ src/Classification.java
@@ -1,46 +1,36 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Classification {
 	
 private void lire() throws FileNotFoundException,UnsupportedEncodingException,IOException{
-	//read the text file to be classified 
-	//
-	ArrayList<String> VecteurFichier =new ArrayList<>();
- 	
- 	FileInputStream FichierALire= new FileInputStream("lien");
+	StopWord st =new StopWord("fichier a classer");
+	
+	ArrayList<String>list=st.EliminerStopWord();
+	//etape de victorization
+	Map <String,Integer> mapfortext=new HashMap<String,Integer>();
+	for(int i=0; i<list.size();i++){
+		mapfortext.put(list.get(i), 1);
 		
- 	InputStreamReader LecteurDeFichier= new InputStreamReader(FichierALire, "UTF-8");
- 	
- 	
- 	//lecture de fichier:
- 	
- 	BufferedReader br = new BufferedReader(LecteurDeFichier);
-		
-	    
-	    
-		String ligne = null;
-
-		while( (ligne = br.readLine())!= null ){
-		        // \\s+ means any number of whitespaces between tokens
-		    String [] tokens = ligne.split("\\s+");
-		    for(int i=0;i< tokens.length;i++){
-		    	
-		    	VecteurFichier.add(tokens[i]);
-		    
-		    }
-//		    String var_1 = tokens[0];
-//		    String var_2 = tokens[1];
-//		    String var_3 = tokens[2];
-		    }
- 	
-
+	}
+	
+	for(int k=0;k<list.size();k++){
+		for(int j=0;j<list.size();j++){
+			
+			if(list.get(k).equals(list.get(j))){
+				mapfortext.put(list.get(k), mapfortext.get(list.get(k))+1);
+				list.remove(k);
+			}
+		}
+	}
+	
+	//etape de tri
+	
  	
  	
  	
